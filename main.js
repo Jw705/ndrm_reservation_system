2540decae0c0669d563a547789baa92c0c175bea
Juwan <sirius_75@naver.com>
2022-08-11 22:20:33 +0900

    resv page 추가, 정적 css, js 연결

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ const session = require('express-session')
 const bodyParser = require('body-parser');
 const FileStore = require('session-file-store')(session)
 
-var authRouter = require('./lib/auth');
-var authCheck = require('./lib/authCheck.js');
+var authRouter = require('./lib/auth');               // 로그인 관련 페이지 연결
+var resvRouter = require('./lib/resv');               // 예약 관련 페이지 연결
+var authCheck = require('./lib/authCheck.js');        // 로그인 여부 판단
 var template = require('./lib/template.js');
 
 const app = express()
 const port = 3000
 
+app.use(express.static('public'));                    // 정적폴더 사용
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
   secret: '~~~',
@@ -24,14 +26,16 @@ app.get('/', (req, res) => {
     res.redirect('/auth/login');
     return false;
   } else {                                      // 로그인 되어있으면 메인 페이지로 이동시킴
-    res.redirect('/main');
+    res.redirect('/resv/main');
     return false;
   }
 })
 
 // 인증 라우터
 app.use('/auth', authRouter);
+app.use('/resv', resvRouter);
 
+/*
 // 메인 페이지
 app.get('/main', (req, res) => {
   if (!authCheck.isOwner(req, res)) {  // 로그인 안되어있으면 로그인 페이지로 이동시킴
@@ -46,6 +50,7 @@ app.get('/main', (req, res) => {
   );
   res.send(html);
 })
+*/
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
