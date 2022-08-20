3d51391ab1124dd9514ff84f118effde32ca2091
Juwan <sirius_75@naver.com>
2022-08-20 21:29:08 +0900

    Change MySQL Server to MS Azure and Add 예약확인시 주차 구분

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var authCheck = require('./lib/authCheck.js');        // 로그인 여부 판단
 var template = require('./lib/template.js');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));                    // 정적폴더 사용
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,24 +35,6 @@ app.get('/', (req, res) => {
 app.use('/resv', resvRouter);
 app.use('/kht', khtRouter);
 
-/*
-// 메인 페이지
-app.get('/main', (req, res) => {
-  if (!authCheck.isOwner(req, res)) {  // 로그인 안되어있으면 로그인 페이지로 이동시킴
-    res.redirect('/auth/login');
-    return false;
-  }
-  var html = template.HTML('Welcome',
-    `<hr>
-        <h2>메인 페이지에 오신 것을 환영합니다</h2>
-        <p>로그인에 성공하셨습니다.</p>`,
-    authCheck.statusUI(req, res)
-  );
-  res.send(html);
-})
-*/
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
